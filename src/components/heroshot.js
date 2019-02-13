e07861ea0b6623d4da000015a8a08de0baee3108
João Pereira <joaopereirawd@gmail.com>
2019-02-13 11:43:11 +0000

    upd: frontend

--- src/components/heroshot.js
+++ src/components/heroshot.js
@@ -44,7 +44,7 @@ class morphHeader extends Component {
                         <span>The easy way to inform users that your website is using cookies. </span>
                     </h1>
                     <button className="btn-custom" onClick={() => document.querySelector('.menu-wrapper').click() }>Customize now</button>
-                    <small className="version">V1.0.0</small>
+                    <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CK7DLK3E&placement=joaopereirawdgithubio" id="_carbonads_js"></script>
 
                 </div>
                 <svg version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 1364 600">
